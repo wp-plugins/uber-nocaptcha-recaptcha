c428cd244fdb2c1478c11feb85413b90cc421bbf
cristian.raiber <cristian.raiber@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-10 21:43:12 +0000

    Removed a folder added by mistake :|
    git-svn-id: https://plugins.svn.wordpress.org/uber-nocaptcha-recaptcha/trunk@1109823 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- tags/1.0/js/render_recaptcha.js
+++
@@ -1,7 +0,0 @@
-var onloadCallback = function () {
-    widgetId2 = grecaptcha.render( 'wdm-nocapt-recapt-id', {
-        'sitekey': wdm_recaptcha.sitekey,
-        'theme': wdm_recaptcha.theme
-    } );
-
-};
